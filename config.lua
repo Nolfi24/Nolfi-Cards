905074fa9cfa22a6d81774a2998aca51feb57eca
Nolfi <sourcebans.nolfi@gmail.com>
2021-04-01 17:06:14 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.ServerName = "Stix Network"
+
+Config.backgroundImage = "https://i.imgur.com/716J5KY.jpg"
+Config.ServerLogo = "https://media.discordapp.net/attachments/789171527311687770/809341830587088906/Ingame_logo.png"
+
+Config.Maxplayers = "64"
+
+Config.Button1 = "JOIN SERVER"
+Config.Button2 = "HJEMMESIDE"
+Config.Button3 = "DISCORD"
+
+Config.Website = "https://Nolfi.Dev/"
+Config.Discord = "https://discord.gg/tyw6JeXB"
